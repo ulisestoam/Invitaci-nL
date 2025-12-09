311f6c34cb60e9211411da246b239a73bb12308d
ulisestoam <45046790+ulisestoam@users.noreply.github.com>
2025-12-09 00:14:54 -0600

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Cuenta días, horas, minutos y segundos hasta la fiesta
   // ========================================
   function updateCountdown() {
-    const weddingDate = new Date('2025-12-06T14:00:00').getTime();
+    const weddingDate = new Date('2025-12-31T14:00:00').getTime();
     const now = new Date().getTime();
     const distance = weddingDate - now;
 
